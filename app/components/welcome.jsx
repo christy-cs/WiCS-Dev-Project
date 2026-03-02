26092d83c003fc642d9760a8019a561a6dc85f24
christy-cs <christyrd.17@gmail.com>
2026-03-02 17:12:04 -0600

    update

---
+++ app/components/welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Welcome() {
+    return(
+        <div>
+            <h1>Hello! Welcome to my page</h1>
+            <p>
+                My name is Christina Rojas-Diaz. I am a computer science major at the University of Illinois, Chicago. <br></br>
+                
+            </p>
+        </div>
+    );
+}
